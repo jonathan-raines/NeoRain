cfd8a81dfc405fda8b6eddb2282ce23ad7ce3677
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-06-14 16:10:33 -0500

    Added lsp-config and lsp-install successfully

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
 require('plugins')
+
+require'lspinstall'.setup() -- important
+
+local servers = require'lspinstall'.installed_servers()
+for _, server in pairs(servers) do
+  require'lspconfig'[server].setup{}
+end
