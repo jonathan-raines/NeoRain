5c156c415c98b23a6fb71fd765775eaab5273f0f
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-06-15 10:30:48 -0500

    Refactored FloatTerm to new file

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require('settings')
 require('plugins')
 require('autopairs')
+require('floatterm')
 require('gitsigns')
 require('lsp-install')
 require('lsp-config')
@@ -538,39 +539,4 @@ require'colorizer'.setup(
 
 -- TODO Comments
 require("todo-comments").setup{}
-
--- Float Term
-require'FTerm'.setup({
-    dimensions  = {
-        height = 0.8,
-        width = 0.8,
-        x = 0.5,
-        y = 0.5
-    },
-    border = 'single' -- or 'double'
-})
-
--- Keybinding
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
--- Closer to the metal
-map('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>', opts)
-map('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
-
-local term = require("FTerm.terminal")
-
-local lazy = term:new():setup({
-    cmd = "lazygit",
-    dimensions = {
-        height = 0.9,
-        width = 0.9
-    }
-})
-
- -- Use this to toggle gitui in a floating terminal
-function _G.__fterm_lazygit()
-    lazy:toggle()
-end
-map('n', '<A-l>', '<CMD>lua _G.__fterm_lazygit()<CR>', opts)
 
---
+++ lua/floatterm/init.lua
@@ -0,0 +1,36 @@
+require'FTerm'.setup({
+    dimensions  = {
+        height = 0.8,
+        width = 0.8,
+        x = 0.5,
+        y = 0.5
+    },
+    border = 'single' -- or 'double'
+})
+
+-- Keybinding
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Closer to the metal
+map('n', '<A-i>', '<CMD>lua require("FTerm").toggle()<CR>', opts)
+map('t', '<A-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
+
+
+-- Create LazyGit Terminal
+local term = require("FTerm.terminal")
+local lazy = term:new():setup({
+    cmd = "lazygit",
+    dimensions = {
+        height = 0.9,
+        width = 0.9
+    }
+})
+
+ -- Use this to toggle gitui in a floating terminal
+function _G.__fterm_lazygit()
+    lazy:toggle()
+end
+map('n', '<A-l>', '<CMD>lua _G.__fterm_lazygit()<CR>', opts)
+
+
