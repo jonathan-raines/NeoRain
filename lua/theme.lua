fd37433929a1ef397bb831c13c43ab36fb073192
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-06-15 11:10:25 -0500

    Finished Refactoring

---
+++ lua/theme.lua
@@ -0,0 +1,15 @@
+-- One Dark
+require('onedark').setup()
+vim.g.onedark_style = 'darker'
+
+-- Tokyo Night
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+
+-- Load the colorscheme
+-- vim.cmd[[colorscheme onedark]]
+vim.cmd[[colorscheme tokyonight]]
+
