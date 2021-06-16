e2fd50ceea07c9c0125dc30326d9dea7eb2f943f
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-06-16 08:54:00 -0500

    Pyright Set up Properly

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -1,1 +1,20 @@
-require'lspconfig'.pyright.setup{}
+require'lspconfig'.pyright.setup {
+    cmd = {DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio"},
+    handlers = {
+        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = true
+        })
+    },
+	 settings = {
+      python = {
+        analysis = {
+		  typeCheckingMode = true,
+		  autoSearchPaths = true,
+          useLibraryCodeForTypes = true 
+        }
+      }
+    }
+}
