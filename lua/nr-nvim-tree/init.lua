a6287b87cd305a30c691bc1bbd5402340e46627a
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-06-17 10:27:38 -0500

    Added dotfiles shown on default

--- lua/nr-nvim-tree/init.lua
+++ lua/nr-nvim-tree/init.lua
@@ -1,8 +1,7 @@
-vim.g.nvim_tree_hide_dotfiles = 1 -- 0 by default, this option hides files and folders starting with a dot `.`
+vim.g.nvim_tree_hide_dotfiles = 0 -- 0 by default, this option hides files and folders starting with a dot `.`
 vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_follow = 1 -- "0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_close = 0 -- 0 by default, closes the tree when it's the last window
-vim.g.nvim_tree_auto_ignore_ft = 'startify' --empty by default, don't auto open tree on specific filetypes.
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
     vim.g.nvim_tree_bindings = {
@@ -40,7 +39,7 @@ local tree_cb = require'nvim-tree.config'.nvim_tree_callback
       ["-"]              = tree_cb("dir_up"),
       ["q"]              = tree_cb("close"),
     }
--- vim.g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1}
+
 vim.g.nvim_tree_icons = {
     default = '',
     symlink = '',
