94b9277f235f42ed61596d8674a58f32bf6e38aa
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-06-17 14:44:50 -0500

    Fixed vim global undefined error

--- lua/lsp/lua-ls.lua
+++ lua/lsp/lua-ls.lua
@@ -1,5 +1,12 @@
 local sumneko_path = "/home/jraines/.local/share/nvim/lspinstall/lua/sumneko-lua-language-server"
 
 require'lspconfig'.sumneko_lua.setup {
-    cmd = {sumneko_path}
+    cmd = {sumneko_path},
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
 }
