35e987800ff2dcdfe995ec5a81bd3a8950fcbb1d
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-06-17 10:27:11 -0500

    Removed some unused global variables

--- lua/nr-globals.lua
+++ lua/nr-globals.lua
@@ -3,15 +3,7 @@ DATA_PATH = vim.fn.stdpath('data')
 CACHE_PATH = vim.fn.stdpath('cache')
 
 O = {
-    auto_close_tree = 0,
-    auto_complete = true,
     colorscheme = 'tokyonight',
-    hidden_files = true,
-    wrap_lines = false,
-    number = true,
-    relative_number = true,
-    shell = 'bash',
-	timeoutlen = 100,
     nvim_tree_disable_netrw = 0,
     extras = false,
     leader_key = ' ',
