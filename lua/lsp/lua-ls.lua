dbf328bcd4309e9c30def0a335162a7770b8f878
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-06-18 09:27:37 -0500

    Deleted useless LOC

--- lua/lsp/lua-ls.lua
+++ lua/lsp/lua-ls.lua
@@ -1,5 +1,3 @@
-local sumneko_path = "/Users/jonathanraines/.local/share/nvim/lspinstall/lua/sumneko-lua-language-server"
-
 require'lspconfig'.sumneko_lua.setup {
     cmd = {DATA_PATH .. "/lspinstall/lua/sumneko-lua-language-server"},
     settings = {
--- lua/nr-compe/init.lua
+++ lua/nr-compe/init.lua
@@ -68,11 +68,3 @@ vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-
---inoremap <silent><expr> <C-Space> compe#complete()
---inoremap <silent><expr> <CR>      compe#confirm('<CR>')
---inoremap <silent><expr> <C-e>     compe#close('<C-e>')
---inoremap <silent><expr> <C-f>     compe#scroll({ 'delta': +4 })
---inoremap <silent><expr> <C-d>     compe#scroll({ 'delta': -4 })
-
-
