2d256c9d39a0dcdf67dfdcc4cba478a86582d6d7
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-08-31 12:51:02 -0500

    Added RestNvim

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,5 +1,14 @@
 local border =  { "", "", "", " ", "", "", "", " " }
 
+local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
+parser_configs.http = {
+  install_info = {
+    url = "https://github.com/NTBBloodbath/tree-sitter-http",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+}
+
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'maintained',
   ignore_installed = {'haskell'},
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -220,4 +220,25 @@ require('packer').startup(function()
     event = 'UIEnter',
     requires = { 'popup.nvim', 'plenary.nvim'}
   }
+
+  use {
+    'NTBBloodbath/rest.nvim',
+    config = function()
+      require("rest-nvim").setup({
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Highlight request on run
+        highlight = {
+            enabled = true,
+            timeout = 150,
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        })
+    vim.api.nvim_set_keymap('n', 'Q', "<cmd>lua require'rest-nvim'.run()<cr>", {noremap = true, silent = true})
+    end,
+    requires = { 'nvim-lua/plenary.nvim' },
+}
 end)
