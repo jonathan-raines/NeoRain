95ba8d0b4842f7b8b1f5e542688a80450219d9e3
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-09-03 12:02:40 -0500

    Prevents code action error on lsp that don't suppport code action

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -13,6 +13,7 @@ local sources = {
   null_ls.builtins.diagnostics.eslint_d,
   null_ls.builtins.formatting.yapf,
   null_ls.builtins.diagnostics.flake8,
+  null_ls.builtins.code_actions.gitsigns,
 }
 
 require('null-ls').config {
