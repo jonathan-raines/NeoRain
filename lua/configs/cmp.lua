81d38cd6185c1a9780ee00ba5f3ed9210094b1a4
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-10-11 09:19:37 -0500

    Removed broken tab functionality

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -46,39 +46,21 @@ cmp.setup {
 
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
   },
 
   mapping = {
-    ['<C-P>'] = cmp.mapping.select_prev_item(),
-    ['<C-N>'] = cmp.mapping.select_next_item(),
-    ['<C-D>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-F>'] = cmp.mapping.scroll_docs(4),
-    ['<C-SPACE>'] = cmp.mapping.complete(),
-    ['<C-E>'] = cmp.mapping.close(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<TAB>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-      elseif require('luasnip').expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-      else
-        fallback()
-      end
-    end,
-    ['<S-TAB>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-      elseif require('luasnip').jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-      else
-        fallback()
-      end
-    end,
   },
 
   sources = {
