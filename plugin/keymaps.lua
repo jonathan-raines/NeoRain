ef9064b85aca2fd3d59f041ba6405a96aa11dbbc
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-10-17 16:53:47 -0500

    Force delete hidden buffers

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -98,7 +98,7 @@ vim.cmd [[function DeleteHiddenBuffers() " Vim with the 'hidden' option
 		let tpbl=[]
 		call map(range(1, tabpagenr('$')), 'extend(tpbl, tabpagebuflist(v:val))')
 		for buf in filter(range(1, bufnr('$')), 'bufexists(v:val) && index(tpbl, v:val)==-1')
-			silent execute 'bwipeout' buf
+			silent execute 'bwipeout!' buf
 		endfor
 	endfunction]]
 
