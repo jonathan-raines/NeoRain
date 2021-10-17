49d2556fe22a3c53cb1d3319d3f5d3307db9c504
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-10-17 14:25:25 -0500

    Added marks nvim

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -103,6 +103,6 @@ vim.cmd [[function DeleteHiddenBuffers() " Vim with the 'hidden' option
 	endfunction]]
 
 vim.api.nvim_set_keymap('n', '<leader>c', ':bdelete<CR>', { silent = true, noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>C', ':call DeleteHiddenBuffers()<CR>', { silent = true, noremap = true })
+-- vim.api.nvim_set_keymap('n', '<leader>C', ':call DeleteHiddenBuffers()<CR>', { silent = true, noremap = true })
 
 vim.api.nvim_set_keymap('n', '<leader>B', ':ls<CR>:b<Space>', { silent = true, noremap = true })
