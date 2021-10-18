5fa09033b31b5a2de81654611f1c25332955d9d7
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-10-18 16:32:03 -0500

    Updated keybind

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -56,8 +56,8 @@ cmp.setup {
   },
 
   mapping = {
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
 
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
