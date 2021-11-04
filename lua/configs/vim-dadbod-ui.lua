f2063699cb84c75fd8c10eb1d2faa7237d739050
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-11-04 13:59:40 -0500

    Updated configs

--- lua/configs/vim-dadbod-ui.lua
+++ lua/configs/vim-dadbod-ui.lua
@@ -2,8 +2,8 @@ vim.cmd [[ let g:db_ui_auto_execute_table_helpers = 1 ]]
 
 vim.cmd [[
     let g:dbs = [
-    \  { 'name': 'app_manager_develop', 'url': 'postgres://postgres:welcome@localhost:5433/app_manager_backend_development' },
-    \  { 'name': 'app_manager_test', 'url': 'postgres://postgres:welcome@localhost:5433/app_manager_backend_test' },
+    \  { 'name': 'app_manager_develop', 'url': 'postgres://postgres:welcome@localhost:5433/appbuild_portal_development' },
+    \  { 'name': 'app_manager_test', 'url': 'postgres://postgres:welcome@localhost:5433/appbuild_portal_test' },
     \  { 'name': 'auth_develop', 'url': 'postgres://postgres:welcome@localhost:5433/oauth_service_development' },
     \  { 'name': 'auth_test', 'url': 'postgres://postgres:welcome@localhost:5433/oauth_service_test' },
     \  { 'name': 'media_develop', 'url': 'postgres://postgres:welcome@localhost:5433/thrillshare_development' },
