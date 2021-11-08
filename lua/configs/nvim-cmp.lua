d87088a7fe2a9995e5a25f5d176037e060f55665
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-11-08 11:10:40 -0600

    Update cmp config

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -62,33 +62,6 @@ cmp.setup {
       select = true,
     },
     ['<C-Space>'] = cmp.mapping.complete(),
-    -- ['<Tab>'] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif luasnip.expand_or_jumpable() then
-    --     luasnip.expand_or_jump()
-    --   elseif has_words_before() then
-    --     cmp.complete()
-    --   else
-    --     fallback()
-    --   end
-    -- end, {
-    --   'i',
-    --   's',
-    -- }),
-    --
-    -- ['<S-Tab>'] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif luasnip.jumpable(-1) then
-    --     luasnip.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end, {
-    --   'i',
-    --   's',
-    -- }),
   },
 
   sources = {
