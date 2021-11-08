65077ecf314d255c6b9c0afb647c37fe2fad3b0b
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-11-08 11:24:15 -0600

    Updated configs

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -14,10 +14,10 @@ vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagn
 })
 
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = 'single',
+  border = 'rounded',
 })
 
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = 'single',
+  border = 'rounded',
   close_events = { 'CursorMoved', 'BufHidden', 'InsertCharPre' },
 })
--- lua/configs/nvim-toggleterm.lua
+++ lua/configs/nvim-toggleterm.lua
@@ -18,14 +18,9 @@ require('toggleterm').setup {
   direction = 'float',
   close_on_exit = true, -- close the terminal window when the process exits
   shell = vim.o.shell, -- change the default shell
-  -- This field is only relevant if direction is set to 'float'
   float_opts = {
-    -- The border key is *almost* the same as 'nvim_open_win'
-    -- see :h nvim_open_win for details on borders however
-    -- the 'curved' border is a custom border type
-    -- not natively supported but implemented in this plugin.
-    border = 'curved',
-    winblend = 0,
+    border = 'rounded',
+    winblend = 10,
     highlights = {
       border = 'Normal',
       background = 'Normal',
