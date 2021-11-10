b8fd7082f680d41d640a3701a34bec6281eb07a3
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-11-10 15:00:28 -0600

    Fix issue with mappings not always attaching

--- lua/configs/nvim-lsp-installer.lua
+++ lua/configs/nvim-lsp-installer.lua
@@ -3,7 +3,7 @@ local lsp_installer = require 'nvim-lsp-installer'
 local map = vim.api.nvim_buf_set_keymap
 local opts = { noremap = true, silent = true }
 
-local mappings = {
+_G_mappings = {
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc'),
   map(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts),
   map(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts),
@@ -28,31 +28,33 @@ function _G_custom_on_attach(client, bufnr)
   end
 
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.cmd [[
-              augroup lsp_document_highlight
-                autocmd! * <buffer>
-                autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-                autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-              augroup end
+  -- if client.resolved_capabilities.document_highlight then
+  --   vim.cmd [[
+  --             augroup lsp_document_highlight
+  --               autocmd! * <buffer>
+  --               autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+  --               autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+  --             augroup end
+  --
+  --           ]]
+  -- end
 
-            ]]
-  end
-
-  if client.resolved_capabilities.code_lens then
-    vim.cmd [[
-              augroup lsp_document_codelens
-                au! * <buffer>
-                autocmd BufWritePost,CursorHold <buffer> lua vim.lsp.codelens.refresh()
-              augroup end
-            ]]
-  end
+  -- if client.resolved_capabilities.code_lens then
+  --   vim.cmd [[
+  --             augroup lsp_document_codelens
+  --               au! * <buffer>
+  --               autocmd BufWritePost,CursorHold <buffer> lua vim.lsp.codelens.refresh()
+  --             augroup end
+  --           ]]
+  -- end
 
   require('which-key').register({
     g = {
       ['a'] = 'Code Action',
       ['b'] = 'Comment Block',
+      ['bc'] = 'Comment Block',
       ['c'] = 'Comment Line',
+      ['cc'] = 'Comment Line',
       ['d'] = 'Definition',
       ['D'] = 'Declaration',
       ['e'] = 'Line Diagnostics',
@@ -65,7 +67,7 @@ function _G_custom_on_attach(client, bufnr)
     },
   }, opts)
 
-  return mappings
+  return _G_mappings
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -143,7 +145,7 @@ lsp_installer.on_server_ready(function(server)
 
         -- no default maps, so you may want to define some here
         local opts3 = { silent = true }
-        table.insert(mappings, {
+        table.insert(_G_mappings, {
           map(bufnr, 'n', 'gs', ':TSLspOrganize<CR>', opts3),
           map(bufnr, 'n', 'gr', ':TSLspRenameFile<CR>', opts3),
           map(bufnr, 'n', 'gi', ':TSLspImportAll<CR>', opts3),
