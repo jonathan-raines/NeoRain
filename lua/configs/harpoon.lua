3b2a470efc5bcbd5c0b57c1c74ed70dd9f02298b
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-11-12 11:58:41 -0600

    Updated harpoon config

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -1,21 +1,23 @@
 require('harpoon').setup {
   global_settings = {
-    save_on_toggle = false,
-    save_on_change = true,
+    save_on_toggle = true,
+    save_on_change = false,
     enter_on_sendcmd = true,
   },
 }
 
 require('which-key').register({
-  j = {
+  h = {
     name = 'Harpoon',
     ['p'] = { '<cmd>lua require("harpoon.mark").add_file()<CR>', 'Add File' },
     ['a'] = { '<cmd>lua require("harpoon.ui").nav_file(1)<CR>', 'Go to file 1' },
     ['s'] = { '<cmd>lua require("harpoon.ui").nav_file(2)<CR>', 'Go to file 2' },
     ['d'] = { '<cmd>lua require("harpoon.ui").nav_file(3)<CR>', 'Go to file 3' },
     ['f'] = { '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', 'Go to file 4' },
-    ['q'] = { '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', 'Quick Menu' },
-    ['t'] = { '<cmd>lua require("harpoon.term").gotoTerminal(1)<CR>', 'Go to Terminal' },
+    ['q'] = { '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', 'Quick Files Menu' },
+    ['w'] = { '<cmd>lua require("harpoon.cmd-ui").toggle_quick_menu()<CR>', 'Quick Commands Menu' },
+    ['t'] = { '<cmd>lua require("harpoon.term").gotoTerminal(1)<CR>', 'Go to Terminal 1' },
+    ['y'] = { '<cmd>lua require("harpoon.term").gotoTerminal(2)<CR>', 'Go to Terminal 2' },
   },
 }, {
   prefix = '<leader>',
