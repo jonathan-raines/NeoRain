9ce69e22723114a84a7f78909c47895ec835fbaa
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-11-23 10:41:13 -0600

    Fix harpoon quickmenu not saving

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -1,7 +1,7 @@
 require('harpoon').setup {
   global_settings = {
-    save_on_toggle = true,
-    save_on_change = false,
+    save_on_toggle = false,
+    save_on_change = true,
     enter_on_sendcmd = true,
   },
 }
