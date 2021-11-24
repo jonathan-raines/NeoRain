9939ba77f26d392feb777f28122d8f65f6ae826d
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-11-24 14:22:50 -0600

    Added incremental selection

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -16,6 +16,15 @@ require('nvim-treesitter.configs').setup {
     enable = true,
     enable_autocmd = false,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      scope_incremental = '<CR>',
+      node_incremental = '<TAB>',
+      node_decremental = '<S-TAB>',
+    },
+  },
   indent = {
     enable = true,
   },
