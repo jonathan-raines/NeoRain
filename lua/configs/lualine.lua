29e231e7b62ab1dc6ee7c220e2c102797842e5dc
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-12-10 08:57:04 -0600

    Update deprecated function

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -80,7 +80,7 @@ require('lualine').setup {
     lualine_x = {
       {
         'diagnostics',
-        sources = { 'nvim_lsp' },
+        sources = { 'nvim_diagnostic' },
         symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
         cond = nil,
       },
