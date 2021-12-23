5588c975d16eeb1fde2ecc67b8ad55d4dec6981d
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-12-23 15:22:19 -0600

    Change options to a table

--- plugin/options.lua
+++ plugin/options.lua
@@ -1,39 +1,49 @@
----@diagnostic disable: undefined-global
-local opt = vim.opt
+local options = {
+  backup = false, -- creates a backup file
+  clipboard = 'unnamedplus', -- allows neovim to access the system clipboard
+  cmdheight = 2, -- more space in the neovim command line for displaying messages
+  colorcolumn = '100', -- creates column at number specified
+  completeopt = { 'menuone', 'noselect' }, -- mostly just for cmp
+  conceallevel = 0, -- so that `` is visible in markdown files
+  cursorline = true, -- highlight the current line
+  expandtab = true, -- convert tabs to spaces
+  fileencoding = 'utf-8', -- the encoding written to a file
+  foldexpr = 'nvim_treesitter#foldexpr()',
+  foldlevelstart = 99,
+  foldmethod = 'expr',
+  hlsearch = false, -- highlight all matches on previous search pattern
+  ignorecase = true, -- ignore case in search patterns
+  mouse = 'a', -- allow the mouse to be used in neovim
+  number = true,
+  numberwidth = 4, -- set number column width to 2 {default 4}
+  pumheight = 10, -- pop up menu height
+  relativenumber = true,
+  scrolloff = 8,
+  shiftwidth = 2, -- the number of spaces inserted for each indentation
+  showmode = false, -- we don't need to see things like -- INSERT -- anymore
+  sidescrolloff = 8,
+  signcolumn = 'yes', -- always show the sign column, otherwise it would shift the text each time
+  smartcase = true, -- smart case
+  smartindent = true, -- make indenting smarter again
+  splitbelow = true, -- force all horizontal splits to go below current window
+  splitright = true, -- force all vertical splits to go to the right of current window
+  swapfile = false, -- creates a swapfile
+  tabstop = 2, -- insert 2 spaces for a tab
+  termguicolors = true, -- set term gui colors (most terminals support this)
+  timeoutlen = 1000, -- time to wait for a mapped sequence to complete (in milliseconds)
+  undofile = true, -- enable persistent undo
+  updatetime = 300, -- faster completion (4000ms default)
+  wildignore = '**/node_modules/*, **/.git/*',
+  wildmode = 'longest:full,full',
+  writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  wrap = false, -- display lines as one long line
+}
 
-opt.clipboard = 'unnamedplus'
-opt.colorcolumn = '100' -- performance penalty when turned on
-opt.completeopt = { 'menu', 'menuone', 'noselect' }
-opt.cursorline = true -- performance penalty when turned on
-opt.expandtab = true
-opt.foldexpr = 'nvim_treesitter#foldexpr()'
-opt.foldmethod = 'expr'
-opt.foldlevelstart = 99
-opt.hlsearch = false
-opt.ignorecase = true
-opt.mouse = 'a'
-opt.number = true
-opt.relativenumber = true
-opt.scrolloff = 8
-opt.shiftwidth = 2
-opt.shortmess:append 'cI'
-opt.sidescrolloff = 8
-opt.signcolumn = 'yes'
-opt.smartcase = true
-opt.splitbelow = true
-opt.splitright = true
-opt.swapfile = false
-opt.tabstop = 2
-opt.termguicolors = true
-opt.timeoutlen = 100
-opt.title = true
-opt.undodir = vim.fn.stdpath 'cache' .. '/undo'
-opt.undofile = true
-opt.updatetime = 3000
-opt.wildmenu = true
-opt.wildmode = 'longest:full,full'
-opt.wildignore = '**/node_modules/*, **/.git/*'
-opt.wrap = false
+vim.opt.shortmess:append 'cI'
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
 -- Highlight on yank
 vim.api.nvim_exec(
