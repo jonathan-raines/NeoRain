3020ef021409feada58362e260bd3c78d5243580
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-12-25 00:06:52 -0600

    Enable profiler for impatient.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -45,7 +45,7 @@ return packer.startup(function(use)
   use {
     'lewis6991/impatient.nvim',
     config = function()
-      require 'impatient'
+      require('impatient').enable_profile()
     end,
   }
 
