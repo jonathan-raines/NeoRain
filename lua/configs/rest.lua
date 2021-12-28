19b9eeffe95dd5b5873a8e22c9ec27ee50c0bbd8
Jonathan Raines <jonathan.s.raines@gmail.com>
2021-12-28 16:32:33 -0600

    Changed keymap due to new default behavior of Q

--- lua/configs/rest.lua
+++ lua/configs/rest.lua
@@ -13,4 +13,4 @@ rest_nvim.setup {
   jump_to_request = true,
 }
 
-vim.api.nvim_set_keymap('n', 'Q', "<cmd>lua require'rest-nvim'.run()<CR>", { silent = true, noremap = true })
+vim.api.nvim_set_keymap('n', '<C-q>', "<cmd>lua require'rest-nvim'.run()<CR>", { silent = true, noremap = true })
