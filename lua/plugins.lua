e42330eaff241a5d5bafde741be7f8ead69939c8
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-01-04 08:45:39 -0600

    Add docker-transformation to vim-test

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -233,6 +233,15 @@ return packer.startup(function(use)
   use {
     'vim-test/vim-test',
     config = function()
+      vim.api.nvim_exec(
+        [[
+          function! DockerTransform(cmd)
+            return "docker-compose exec $(tmux display-message -p '#S') " .a:cmd
+          endfunction]],
+        false
+      )
+      vim.cmd [[ let test#custom_transformations = {'docker': function('DockerTransform')}]]
+      vim.cmd [[ let test#transformation = 'docker']]
       vim.cmd [[ let test#strategy = "neovim" ]]
     end,
     cmd = { 'TestFile', 'TestLast', 'TestNearest', 'TestSuite', 'TestVisit' },
