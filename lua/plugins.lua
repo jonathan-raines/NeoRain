74ec9b42d144a3c84fb1ca45657fc036999bab35
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-01-14 12:27:26 -0600

    Remove vim-rails

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -167,11 +167,6 @@ return packer.startup(function(use)
     requires = 'nvim-lua/plenary.nvim',
     ft = { 'dart' },
   }
-
-  use {
-    'tpope/vim-rails',
-    ft = { 'ruby' },
-  }
   ---------------------------------------
 
   ---------- LSP ----------
