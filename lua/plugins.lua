e1eae24047a1ca85b178969f52039b6ae846e7b5
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-02-04 09:15:31 -0600

    Move textobjects config to it's plugins config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -271,6 +271,60 @@ return packer.startup(function(use)
 
   use {
     'nvim-treesitter/nvim-treesitter-textobjects',
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        textobjects = {
+          lsp_interop = {
+            enable = true,
+            border = 'rounded',
+            peek_definition_code = {
+              ['df'] = '@function.outer',
+              ['dF'] = '@class.outer',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+          },
+          select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
+          },
+          swap = {
+            enable = false,
+            swap_next = {
+              ['<leader>a'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>A'] = '@parameter.inner',
+            },
+          },
+        },
+      }
     after = 'nvim-treesitter',
   }
 
