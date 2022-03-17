f44c527be8d7997e5f34f8679ce4f2f47262cb76
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-03-17 11:38:51 -0500

    Update harpoon config

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -7,10 +7,10 @@ require('harpoon').setup {
     save_on_change = true,
 
     -- sets harpoon to run the command immediately as it's passed to the terminal when calling `sendCommand`.
-    enter_on_sendcmd = false,
+    enter_on_sendcmd = true,
 
     -- closes any tmux windows harpoon that harpoon creates when you close Neovim.
-    tmux_autoclose_windows = false,
+    tmux_autoclose_windows = true,
 
     -- filetypes that you want to prevent from adding to the harpoon list menu.
     excluded_filetypes = { 'harpoon' },
