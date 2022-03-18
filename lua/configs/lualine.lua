e32324796eaa2fb9087c5cbab46ef2dc56575515
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-03-18 07:30:35 -0500

    Add global status line

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -44,6 +44,7 @@ require('lualine').setup {
     section_separators = { left = '', right = '' },
     disabled_filetypes = {},
     always_divide_middle = true,
+    globalstatus = true,
   },
   sections = {
     lualine_a = {
