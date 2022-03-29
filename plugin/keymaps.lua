502b0c673f613306b437c6c7f9b8a814fd2c449f
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-03-29 16:19:09 -0500

    Update keymaps config

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -149,30 +149,3 @@ function EscapePair()
     vim.api.nvim_win_set_cursor(0, { row, col + 1 })
   end
 end
-
--- vim.api.nvim_exec(
---   [[
---     :function StayCenteredI()
---     :  let line = line(".")
---     :  if line != get(b:, 'last_line', 0)
---     :    let col = getcurpos()[4]
---     :    normal! zz
---     :    call cursor(line, col)
---     :    let b:last_line = line
---     :  endif
---     :endfunction
---     :function StayCentered()
---     :  let line = line(".")
---     :  if line != get(b:, 'last_line', 0)
---     :    normal! zz
---     :    let b:last_line = line
---     :  endif
---     :endfunction
---     augroup StayCentered
---       autocmd!
---       autocmd CursorMovedI * :call StayCenteredI()
---       autocmd CursorMoved * :call StayCentered()
---     augroup END
---   ]],
---   true
--- )
