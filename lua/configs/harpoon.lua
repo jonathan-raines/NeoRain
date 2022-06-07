a4be6436ea4a8fcc1399b2917664e988bb94213e
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-06-07 15:12:21 -0500

    Pcall external plugins in config

--- lua/configs/harpoon.lua
+++ lua/configs/harpoon.lua
@@ -6,19 +6,22 @@ require 'harpoon'.setup {
   },
 }
 
-require 'which-key'.register({
-  h = {
-    name = 'Harpoon',
-    ['p'] = { '<cmd>lua require("harpoon.mark").add_file()<CR>', 'Add File' },
-    ['f'] = { '<cmd>lua require("harpoon.ui").nav_file(1)<CR>', 'Go to file 1' },
-    ['d'] = { '<cmd>lua require("harpoon.ui").nav_file(2)<CR>', 'Go to file 2' },
-    ['s'] = { '<cmd>lua require("harpoon.ui").nav_file(3)<CR>', 'Go to file 3' },
-    ['a'] = { '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', 'Go to file 4' },
-    ['q'] = { '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', 'Files Menu' },
-    ['c'] = { '<cmd>lua require("harpoon.cmd-ui").toggle_quick_menu()<CR>', 'Commands Menu' },
-    ['t'] = { '<cmd>lua require("harpoon.term").gotoTerminal(1)<CR>', 'Go to Terminal 1' },
-    ['r'] = { '<cmd>lua require("harpoon.term").gotoTerminal(2)<CR>', 'Go to Terminal 2' },
-  },
-}, {
-  prefix = '<leader>',
-})
+local wk_ok, wk = pcall(require, 'which-key')
+if wk_ok then
+  wk.register({
+    h = {
+      name = 'Harpoon',
+      ['p'] = { '<cmd>lua require("harpoon.mark").add_file()<CR>', 'Add File' },
+      ['f'] = { '<cmd>lua require("harpoon.ui").nav_file(1)<CR>', 'Go to file 1' },
+      ['d'] = { '<cmd>lua require("harpoon.ui").nav_file(2)<CR>', 'Go to file 2' },
+      ['s'] = { '<cmd>lua require("harpoon.ui").nav_file(3)<CR>', 'Go to file 3' },
+      ['a'] = { '<cmd>lua require("harpoon.ui").nav_file(4)<CR>', 'Go to file 4' },
+      ['q'] = { '<cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', 'Files Menu' },
+      ['c'] = { '<cmd>lua require("harpoon.cmd-ui").toggle_quick_menu()<CR>', 'Commands Menu' },
+      ['t'] = { '<cmd>lua require("harpoon.term").gotoTerminal(1)<CR>', 'Go to Terminal 1' },
+      ['r'] = { '<cmd>lua require("harpoon.term").gotoTerminal(2)<CR>', 'Go to Terminal 2' },
+    },
+  }, {
+    prefix = '<leader>',
+  })
+end
