8845c7f83b67829693cf8b12a03fbbe5fba838d5
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-06-08 12:58:41 -0500

    formatting

--- lua/configs/comment.lua
+++ lua/configs/comment.lua
@@ -1,4 +1,4 @@
-require('Comment').setup {
+require 'Comment'.setup {
   ignore = '^$',
   pre_hook = function(ctx)
     -- Only calculate commentstring for tsx filetypes
@@ -11,12 +11,12 @@ require('Comment').setup {
       -- Determine the location where to calculate commentstring from
       local location = nil
       if ctx.ctype == U.ctype.block then
-        location = require('ts_context_commentstring.utils').get_cursor_location()
+        location = require 'ts_context_commentstring.utils'.get_cursor_location()
       elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require('ts_context_commentstring.utils').get_visual_start_location()
+        location = require 'ts_context_commentstring.utils'.get_visual_start_location()
       end
 
-      return require('ts_context_commentstring.internal').calculate_commentstring {
+      return require 'ts_context_commentstring.internal'.calculate_commentstring {
         key = type,
         location = location,
       }
