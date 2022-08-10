27f4cee7ede2a6ec0c04877c70b58ccbd5464c54
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-08-09 22:48:00 -0500

    Update lualine config

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -71,6 +71,43 @@ require 'lualine'.setup {
       { 'tabs' },
     },
   },
-  tabline = {},
+  winbar = {
+    lualine_a = {
+      {
+        'filename',
+        file_status = true, -- Displays file status (readonly status, modified status)
+        newfile_status = true, -- Display new file status (new file means no write after created)
+        path = 0,
+        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+        -- for other components. (terrible name, any suggestions?)
+        symbols = {
+          modified = '[+]', -- Text to show when the file is modified.
+          readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+          unnamed = '[No Name]', -- Text to show for unnamed buffers.
+          newfile = '[New]', -- Text to show for new created file before first writting
+        },
+        separator = { left = '', right = '' },
+      }
+    }
+  },
+  inactive_winbar = {
+    lualine_a = {
+      {
+        'filename',
+        file_status = true, -- Displays file status (readonly status, modified status)
+        newfile_status = true, -- Display new file status (new file means no write after created)
+        path = 0,
+        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+        -- for other components. (terrible name, any suggestions?)
+        symbols = {
+          modified = '[+]', -- Text to show when the file is modified.
+          readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+          unnamed = '[No Name]', -- Text to show for unnamed buffers.
+          newfile = '[New]', -- Text to show for new created file before first writting
+        },
+        separator = { left = '', right = '' },
+      }
+    }
+  },
   extensions = { 'quickfix', 'toggleterm' },
 }
