3dfde78be7344bfcbb17763e1cf6921fc91ed76e
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-08-11 10:53:21 -0500

    Refactor keymaps

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -9,8 +9,6 @@ keymap('', '<Space>', '<Nop>', opts)
 local keymaps = {
   { { 'i', 'v' }, 'jk', '<ESC>', { desc = 'Return to normal mode' } },
 
-  { { 'n' }, '<leader><space>', '<c-^>', { desc = 'Alternate Buffer' } },
-
   { { 'n' }, '<leader>w', '<cmd>up<CR>', { desc = 'Write' } },
 
   { { 'n' }, '<leader>c', '<cmd>execute (v:count > 0 ? v:count : "") . "bd"<CR>', { desc = 'Close Buffer' } },
@@ -24,19 +22,16 @@ local keymaps = {
   { { 'n' }, '<', '<<', { desc = 'Increase Indent' } },
   { { 'n' }, '>', '>>', { desc = 'Decrease Indent' } },
 
+  { { 'v' }, '<', '<gv', { desc = 'Keep visual selection on indent decrease' } },
+  { { 'v' }, '>', '>gv', { desc = 'Keep visual selection on indent increase' } },
+
   { { 'n' }, '<C-j>', ':m .+1<CR>==', { desc = 'Move line up' } },
   { { 'n' }, '<C-k>', ':m .-2<CR>==', { desc = 'Move line down' } },
-
-  { { 'n' }, '<', '<<', { desc = 'Increase Indent' } },
-  { { 'n' }, '>', '>>', { desc = 'Decrease Indent' } },
-
-  { { 'i' }, '<C-l>', "<cmd>lua require('utils').escape_pair()<CR>", { desc = 'Escape pair' } },
 
   { { 'v' }, '<C-j>', ":m '>+1<CR>gv-gv", { desc = 'Move line up' } },
   { { 'v' }, '<C-k>', ":m '<-2<CR>gv-gv", { desc = 'Move line down' } },
 
-  { { 'v' }, '<', '<gv', { desc = 'Keep visual selection on indent decrease' } },
-  { { 'v' }, '>', '>gv', { desc = 'Keep visual selection on indent increase' } },
+  { { 'i' }, '<C-l>', "<cmd>lua require('utils').escape_pair()<CR>", { desc = 'Escape pair' } },
 }
 
 for _, val in pairs(keymaps) do
