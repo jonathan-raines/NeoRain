b00e614c8c4792a2fb12564569bfd349335b5247
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-08-29 18:45:36 -0500

    Readd util functions

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+M.quick_fix_toggle = function()
+  if vim.fn.empty(vim.fn.filter(vim.fn.getwininfo(), 'v:val.quickfix')) == 1 then
+    vim.cmd 'copen'
+  else
+    vim.cmd 'cclose'
+  end
+end
+
 M.escape_pair = function()
   local closers = { ')', ']', '}', '>', "'", '"', '`', ',' }
   local line = vim.api.nvim_get_current_line()
@@ -40,6 +48,30 @@ M.get_client_names = function()
   end
 
   return table.concat(clientnames_tbl, ', ')
+end
+
+M.lsp_progress = function()
+  local Lsp = vim.lsp.util.get_progress_messages()[1]
+
+  if Lsp then
+    local msg = Lsp.message or ''
+    local percentage = Lsp.percentage or 0
+    local title = Lsp.title or ''
+
+    local spinners = { '', '', '' }
+    local success_icon = { '', '', '' }
+
+    local ms = vim.loop.hrtime() / 1000000
+    local frame = math.floor(ms / 120) % #spinners
+
+    if percentage >= 70 then
+      return string.format(' %%<%s %s %s (%s%%%%) ', success_icon[frame + 1], title, msg, percentage)
+    end
+
+    return string.format(' %%<%s %s %s (%s%%%%) ', spinners[frame + 1], title, msg, percentage)
+  end
+
+  return ''
 end
 
 return M
