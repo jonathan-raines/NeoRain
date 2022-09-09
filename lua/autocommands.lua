150ee7f595ecff6d98c88e0408b979d9bc004ebf
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-09-09 08:15:36 -0500

    Update autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,12 +1,27 @@
 local augroups = {}
 
 augroups.misc = {
+  -- create_missing_directories = {
+  --   desc = 'Create missing directories before saving the buffer',
+  --   event = 'BufWritePre',
+  --   callback = function()
+  --     vim.fn.mkdir(vim.fn.expand '%:p:h', 'p')
+  --   end,
+  -- },
   cursor_location = {
     desc = 'Open file at the last position it was edited earlier',
     event = 'BufReadPost',
     pattern = '*',
     command = 'silent! normal! g`"zv',
   },
+  -- hide_cursorline = {
+  --   desc = 'Hide cursorline in inactive window',
+  --   event = 'WinLeave',
+  --   pattern = '*',
+  --   callback = function()
+  --     vim.opt_local.cursorline = false
+  --   end,
+  -- },
   highlight_yank = {
     desc = 'Highland text on yank',
     event = 'TextYankPost',
@@ -15,26 +30,34 @@ augroups.misc = {
       vim.highlight.on_yank { higroup = 'Visual' }
     end
   },
-  trim_whitespace = {
-    desc = 'Trim whitespace on save',
-    event = 'BufWritePre',
+  newline_comment = {
+    desc = 'No commenting new line',
+    event = 'BufEnter',
     pattern = '*',
-    command = [[:%s/\s\+$//e]],
-  }
+    command = 'set fo-=c fo-=r fo-=o',
+  },
+  -- show_cursorline = {
+  --   desc = 'Show cursorline in active window',
+  --   event = 'WinEnter',
+  --   pattern = '*',
+  --   callback = function()
+  --     vim.opt_local.cursorline = true
+  --   end,
+  -- },
+  -- trim_whitespace = {
+  --   desc = 'Trim whitespace on save',
+  --   event = 'BufWritePre',
+  --   pattern = '*',
+  --   command = [[:%s/\s\+$//e]],
+  -- }
 }
 
 augroups.terminal = {
   close_terminal = {
-    desc = 'Close terminal automatically when exit 0',
+    desc = 'Close terminal with bd',
     event = 'TermClose',
     pattern = '*',
     command = "if !v:event.status | exe 'bdelete! '..expand('<abuf>') | endif",
-  },
-  set_terminal_options = {
-    desc = 'Set terminal options',
-    event = 'TermOpen',
-    pattern = '*',
-    command = 'set nonumber norelativenumber nobuflisted',
   },
   set_keymaps = {
     event = 'TermOpen',
@@ -42,6 +65,18 @@ augroups.terminal = {
     pattern = 'term://*',
     command = "lua require('utils').set_terminal_keymaps()",
   },
+  set_terminal_options = {
+    desc = 'Set terminal options',
+    event = 'TermOpen',
+    pattern = '*',
+    command = 'set nonumber norelativenumber nobuflisted',
+  },
+  -- start_insert_mode = {
+  --   desc = 'Start terminal in insert mode',
+  --   event = 'TermOpen',
+  --   pattern = '*',
+  --   command = 'startinsert',
+  -- }
 }
 
 for group, commands in pairs(augroups) do
