734bf21afe4cdfd3eab50053fde5afec9482a1d5
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-09-11 10:45:59 -0500

    Disable winbar

--- lua/options.lua
+++ lua/options.lua
@@ -41,7 +41,7 @@ local options = {
 vim.opt.shortmess:append 'cI'
 vim.opt.path:append '**'
 vim.o.laststatus = 3
-vim.o.winbar = '%= %m %t'
+-- vim.o.winbar = '%= %m %t'
 
 for k, v in pairs(options) do
   vim.opt[k] = v
