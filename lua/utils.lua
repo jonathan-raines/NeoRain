e749c3b8a696376a30a5fde13b98f1b8f8432a96
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-09-16 08:58:45 -0500

    Update utils

--- lua/utils.lua
+++ lua/utils.lua
@@ -36,43 +36,23 @@ M.set_terminal_keymaps = function()
   vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], term_opts)
 end
 
-M.get_client_names = function()
-  local bufnr = vim.fn.bufnr ''
-  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-  local clients = vim.lsp.buf_get_clients(bufnr)
-  local clientnames_tbl = {}
-
-  for _, v in pairs(clients) do
-    local filetypes = v.config.filetypes
-    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 and v.name then
-      table.insert(clientnames_tbl, v.name)
+M.LSP_status = function()
+  if rawget(vim, 'lsp') then
+    for _, client in ipairs(vim.lsp.get_active_clients()) do
+      if client.attached_buffers[vim.api.nvim_get_current_buf()] then
+        return (vim.o.columns > 100 and '%#Normal#' .. '   ' .. client.name .. ' ')
+      end
     end
   end
-
-  return table.concat(clientnames_tbl, ', ')
+  return ''
 end
 
-M.lsp_progress = function()
-  local Lsp = vim.lsp.util.get_progress_messages()[1]
+M.Treesitter = function()
+  vim.cmd [[hi Treesitter guifg='seagreen']]
 
-  if Lsp then
-    local msg = Lsp.message or ''
-    local percentage = Lsp.percentage or 0
-    local title = Lsp.title or ''
-
-    local spinners = { '', '', '' }
-    local success_icon = { '', '', '' }
-
-    local ms = vim.loop.hrtime() / 1000000
-    local frame = math.floor(ms / 120) % #spinners
-
-    if percentage >= 70 then
-      return string.format(' %%<%s %s %s (%s%%%%) ', success_icon[frame + 1], title, msg, percentage)
-    end
-
-    return string.format(' %%<%s %s %s (%s%%%%) ', spinners[frame + 1], title, msg, percentage)
+  if next(vim.treesitter.highlighter.active) then
+    return '%#Treesitter#' .. ' '
   end
-
   return ''
 end
 
