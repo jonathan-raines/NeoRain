4d178be21b64c9c3e46b028ce181b3c9b8ab8543
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-09-24 15:30:09 -0500

    Linting

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -1,6 +1,4 @@
-local configs = require 'nvim-treesitter.configs'
-
-configs.setup {
+require 'nvim-treesitter.configs'.setup {
   ensure_installed = 'all',
   ignore_installed = { 'haskell' },
   incremental_selection = {
@@ -9,18 +7,16 @@ configs.setup {
       init_selection = '<CR>',
       scope_incremental = '<CR>',
       node_incremental = '<TAB>',
-      node_decremental = '<S-TAB>',
-    },
+      node_decremental = '<S-TAB>'
+    }
   },
-  indent = {
-    enable = true,
-  },
+  indent = { enable = true },
   highlight = {
     enable = true, -- false will disable the whole extension
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+    additional_vim_regex_highlighting = false
+  }
 }
