cfc4506b0f74275ef13b774f82656ef46a5ec352
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-10-12 15:03:56 -0500

    Fix flutter plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -159,6 +159,9 @@ return packer.startup(function(use)
   use {
     'akinsho/flutter-tools.nvim',
     requires = 'nvim-lua/plenary.nvim',
+    config = function()
+      require 'flutter-tools'.setup {}
+    end,
     ft = { 'dart' }
   }
 
