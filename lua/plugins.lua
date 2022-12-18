028c92502b010372e50bb6629dc82f9288cb6762
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-12-18 15:40:26 -0600

    Remove whichkey

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -211,13 +211,6 @@ return packer.startup(function(use)
     end
   }
 
-  -- use {
-  --   'folke/which-key.nvim',
-  --   config = function()
-  --     require 'configs.which-key'.config()
-  --   end
-  -- }
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if is_bootstrap then
