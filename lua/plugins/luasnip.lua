c98b9785e8b789574acf8f4202a87f924b6d1404
Jonathan Raines <jonathan.s.raines@gmail.com>
2022-12-20 21:55:37 -0600

    Add friendly snippets to LuaSnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,6 +1,14 @@
 local M = {
   'L3MON4D3/LuaSnip',
-  dependencies = 'saadparwaiz1/cmp_luasnip',
+  dependencies = {
+    'saadparwaiz1/cmp_luasnip',
+    {
+      'rafamadriz/friendly-snippets',
+      config = function()
+        require 'luasnip.loaders.from_vscode'.lazy_load()
+      end
+    }
+  },
   lazy = true
 }
 
