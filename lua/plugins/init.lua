82e6f9e7f79b100fecf0cebbf631e1d5d83256c6
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-01-06 08:08:22 -0600

    Update plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,35 +1,10 @@
 return {
   {
-    'ThePrimeagen/refactoring.nvim',
-    init = function()
-      -- prompt for a refactor to apply when the remap is triggered
-      vim.keymap.set('v', '<leader>r', function()
-        require 'refactoring'.select_refactor()
-      end, { noremap = true, silent = true, expr = false })
-    end,
-    config = function()
-      require 'refactoring'.setup {}
-    end,
-    enabled = false
-  },
-
-  {
     'Wansmer/treesj',
     cmd = 'TSJToggle',
     config = function()
       require 'treesj'.setup { use_default_keymaps = false }
-    end,
-    enabled = true
-  },
-
-  {
-    'cshuaimin/ssr.nvim',
-    init = function()
-      vim.keymap.set({ 'n', 'x' }, '<leader>cR', function()
-        require 'ssr'.open()
-      end, { desc = 'Structural Replace' })
-    end,
-    enabled = false
+    end
   },
 
   {
@@ -42,4 +17,10 @@ return {
     end
   },
 
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require 'mason'.setup()
+    end
+  }
 }
