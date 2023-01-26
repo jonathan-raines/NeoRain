d652e7298d04f54f35521c0b887ff6e03ab5c756
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-01-26 12:49:18 -0600

    Add catppuccin colorscheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,4 +22,6 @@ return {
   { 'savq/melange-nvim', priority = 1000, lazy = false },
 
   { 'AlexvZyl/nordic.nvim', priority = 1000, lazy = false },
+
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000, lazy = false },
 }
