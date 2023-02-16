64746a5b9d4eece612c2567264e53a7d955d27da
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-02-16 12:36:32 -0600

    Update plugins

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,5 +3,8 @@ return {
   build = function()
     vim.fn['mkdp#util#install']()
   end,
+  keys = {
+    { 'n', '<leader>p', '<cmd>MarkdownPreview<CR>', desc = 'Markdown Preview' }
+  },
   ft = 'markdown'
 }
--- lua/plugins/printer.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'rareitems/printer.nvim',
-  config = function()
-    require 'printer'.setup {
-      keymap = 'gp' -- Plugin doesn't have any keymaps by default
-    }
-  end,
-  keys = 'gp'
-}
