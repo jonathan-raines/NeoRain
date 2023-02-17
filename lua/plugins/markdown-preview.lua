18fcbf34b968b5426b7180adec0fa998f1125200
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-02-17 16:34:12 -0600

    Update plugins

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,8 +3,5 @@ return {
   build = function()
     vim.fn['mkdp#util#install']()
   end,
-  keys = {
-    { 'n', '<leader>p', '<cmd>MarkdownPreview<CR>', desc = 'Markdown Preview' }
-  },
-  ft = 'markdown'
+  ft = 'markdown',
 }
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -73,12 +73,12 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert {
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-b>'] = cmp.mapping.scroll_docs( -4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<C-y>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      },
+        ['<C-y>'] = cmp.mapping.confirm { select = true } , -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      } ,
       snippet = {
         expand = function(args)
           require 'luasnip'.lsp_expand(args.body)
@@ -90,7 +90,7 @@ return {
         { name = 'buffer' },
         { name = 'path' },
         { name = 'nvim_lua' },
-      },
+      } ,
     }
   end,
   event = 'InsertEnter'
