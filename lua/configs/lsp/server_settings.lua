3e9deb7b639be2a448c062aba4d9338313b9394a
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-02-19 09:58:16 -0600

    Fix sumenko settings for deprecated function warnings

--- lua/configs/lsp/server_settings.lua
+++ lua/configs/lsp/server_settings.lua
@@ -4,27 +4,23 @@ M.sumneko_lua = function()
   return {
     settings = {
       Lua = {
-        diagnostics = {
-          globals = { 'vim' },
+        runtime = {
+          version = 'LuaJIT',
+          path = runtime_path,
         },
-        format = {
+        completion = {
+          callSnippet = 'Replace',
+        },
+        diagnostics = {
           enable = true,
-          defaultConfig = {
-            indent_style = 'spaces',
-            indent_size = '2',
-            quote_style = 'single',
-            call_arg_parentheses = 'remove',
-            continuation_indent_size = '2',
-            max_line_length = '140',
-            continuous_assign_table_field_align_to_equal_sign = 'false'
-          },
+          globals = { 'vim', 'use' },
         },
         workspace = {
-          library = {
-            [vim.fn.expand '$VIMRUNTIME/lua'] = true,
-            [vim.fn.stdpath 'config' .. '/lua'] = true,
-          },
+          library = vim.api.nvim_get_runtime_file('', true),
+          maxPreload = 10000,
+          preloadFileSize = 10000,
         },
+        telemetry = { enable = false },
       },
     },
   }
