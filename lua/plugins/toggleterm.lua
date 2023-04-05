959f4a45eddde5b24cd0adbc4b9fe32b47a4d5a8
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-04-05 17:40:52 -0500

    Remove keybind for lazygit

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -13,13 +13,13 @@ return {
       hide_numbers = true, -- hide the number column in toggleterm buffers
       shade_filetypes = {},
       shade_terminals = true,
-      shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+      shading_factor = 1,     -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
       start_in_insert = true,
       insert_mappings = true, -- whether or not the open mapping applies in insert mode
       persist_size = true,
       direction = 'float',
       close_on_exit = true, -- close the terminal window when the process exits
-      shell = vim.o.shell, -- change the default shell
+      shell = vim.o.shell,  -- change the default shell
       float_opts = {
         border = 'rounded',
         winblend = 0,
@@ -27,20 +27,17 @@ return {
           border = 'Normal',
           background = 'Normal',
         },
-        -- height = function()
-        --   return math.floor(0.8 * vim.o.columns)
-        -- end,
       },
     }
 
     local Terminal = require 'toggleterm.terminal'.Terminal
 
     function _Lazygit_toggle()
-      Terminal:new { cmd = 'lazygit', count = 76, hidden = true } :toggle()
+      Terminal:new { cmd = 'lazygit', count = 76, hidden = true }:toggle()
     end
   end,
   keys = {
     { [[<c-\>]] },
-    { '<leader>gl', '<cmd>lua _Lazygit_toggle()<CR>', desc = 'Lazygit' },
+    -- { '<leader>gl', '<cmd>lua _Lazygit_toggle()<CR>', desc = 'Lazygit' },
   }
 }
