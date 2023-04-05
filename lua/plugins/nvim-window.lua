64655cf7e3fba9b128ab3b8b845b1e2a96c9da71
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-04-05 17:43:06 -0500

    Change keybind to open window picker

--- lua/plugins/nvim-window.lua
+++ lua/plugins/nvim-window.lua
@@ -2,6 +2,6 @@ return {
   name = 'nvim-window',
   url = 'https://gitlab.com/yorickpeterse/nvim-window.git',
   keys = {
-    { '<C-p>', "<cmd>lua require 'nvim-window'.pick()<CR>", desc = 'Pick a window' }
+    { '<leader><leader>', "<cmd>lua require 'nvim-window'.pick()<CR>", desc = 'Pick a window' }
   }
 }
