b1b11d2cbf6df365359e037e5834ad0a5277a47f
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-06-26 08:39:48 -0500

    Add git_status fzflua keymap

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -2,13 +2,15 @@ return {
   'ibhagwan/fzf-lua',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   keys = {
-    { "<leader>'", "<cmd>lua require('fzf-lua').resume()<CR>",                    desc = 'Resume Last Picker' },
-    { '<leader>f', "<cmd>lua require('fzf-lua').files()<CR>",                     desc = 'Find Files' },
-    { '<leader>o', "<cmd>lua require('fzf-lua').oldfiles({cwd_only = true})<CR>", desc = 'Old Files' },
-    { '<leader>b', "<cmd>lua require('fzf-lua').buffers()<CR>",                   desc = 'Buffers' },
-    { '<leader>/', "<cmd>lua require('fzf-lua').live_grep_native()<CR>",          desc = 'Live Grep' },
-    { '<leader>j', "<cmd>lua require('fzf-lua').jumps()<CR>",                     desc = 'Jumps' },
-    { '<leader>t', "<cmd>lua require('fzf-lua').tabs()<CR>",                      desc = 'Tabs' },
-    { '/',         "<cmd>lua require('fzf-lua').lgrep_curbuf()<CR>",              desc = "Search Buffer" }
+    { '<leader>ff', "<cmd>FzfLua files<CR>",                                       desc = 'Find Files' },
+    { '<leader>fh', "<cmd>FzfLua git_status<CR>",                                  desc = 'Changed Git Files' },
+    { '<leader>fs', "<cmd>FzfLua grep_cword<CR>",                                  desc = 'Word Under Cursor' },
+    { '<leader>fo', "<cmd>lua require('fzf-lua').oldfiles({cwd_only = true})<CR>", desc = 'Old Files' },
+    { '<leader>fj', "<cmd>FzfLua jumps<CR>",                                       desc = 'Jumps' },
+    { '<leader>b',  "<cmd>FzfLua buffers<CR>",                                     desc = 'Buffers' },
+    { '<leader>t',  "<cmd>FzfLua tabs<CR>",                                        desc = 'Tabs' },
+    { "<leader>'",  "<cmd>FzfLua resume<CR>",                                      desc = 'Resume Last Picker' },
+    { '<leader>/',  "<cmd>FzfLua live_grep_native<CR>",                            desc = 'Live Grep' },
+    { '/',          "<cmd>FzfLua lgrep_curbuf<CR>",                                desc = "Search Buffer" }
   }
 }
