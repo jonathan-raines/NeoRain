34b73c0b4c2853aa74b501ebde2a1f37f949b127
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-07-06 09:03:39 -0500

    Update fzflua keymaps

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,16 +1,31 @@
 return {
   'ibhagwan/fzf-lua',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require 'fzf-lua'.setup {
+      keymap = {
+        fzf = {
+          ["ctrl-q"] = "select-all+accept"
+        }
+      }
+    }
+  end,
   keys = {
     { '<leader>ff', "<cmd>FzfLua files<CR>",                                       desc = 'Find Files' },
     { '<leader>fh', "<cmd>FzfLua git_status<CR>",                                  desc = 'Changed Git Files' },
+    { '<leader>fk', "<cmd>FzfLua keymaps<CR>",                                     desc = 'Keymaps' },
+    { '<leader>fk', "<cmd>FzfLua marks<CR>",                                       desc = 'Marks' },
     { '<leader>fs', "<cmd>FzfLua grep_cword<CR>",                                  desc = 'Word Under Cursor' },
     { '<leader>fo', "<cmd>lua require('fzf-lua').oldfiles({cwd_only = true})<CR>", desc = 'Old Files' },
+    { '<leader>fc', "<cmd>FzfLua colorscheme<CR>",                                 desc = 'Colorscheme' },
     { '<leader>fj', "<cmd>FzfLua jumps<CR>",                                       desc = 'Jumps' },
+    { '<leader>fq', "<cmd>FzfLua quickfix<CR>",                                    desc = 'Quickfix' },
+    { '<leader>fl', "<cmd>FzfLua loclist<CR>",                                     desc = 'Local List' },
+    { "<leader>'",  "<cmd>FzfLua resume<CR>",                                      desc = 'Resume Last Picker' },
     { '<leader>b',  "<cmd>FzfLua buffers<CR>",                                     desc = 'Buffers' },
     { '<leader>t',  "<cmd>FzfLua tabs<CR>",                                        desc = 'Tabs' },
-    { "<leader>'",  "<cmd>FzfLua resume<CR>",                                      desc = 'Resume Last Picker' },
     { '<leader>/',  "<cmd>FzfLua live_grep_native<CR>",                            desc = 'Live Grep' },
     { '/',          "<cmd>FzfLua lgrep_curbuf<CR>",                                desc = "Search Buffer" }
-  }
+  },
+  event = "VeryLazy"
 }
