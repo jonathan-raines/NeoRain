4120b04f9466ba4d36c9baba1da7d65a50fcde93
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-08-26 13:50:48 -0500

    Switch to tokyonight

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,39 +1,51 @@
 return {
   {
-    'navarasu/onedark.nvim',
+    'folke/tokyonight.nvim',
     config = function()
-      local onedark = require 'onedark'
-      onedark.setup {
-        code_style = {
-          comments = 'italic',
-          keywords = 'italic',
-          functions = 'italic',
-          transparent = true
-        },
+      require 'tokyonight'.setup {
+        transparent = true,
+        dim_inactive = true,
+        -- on_highlights = function(hl, c)
+        --   local prompt = "#2d3149"
+        --   hl.TelescopeNormal = {
+        --     bg = c.bg_dark,
+        --     fg = c.fg_dark,
+        --   }
+        --   hl.TelescopeBorder = {
+        --     bg = c.bg_dark,
+        --     fg = c.bg_dark,
+        --   }
+        --   hl.TelescopePromptNormal = {
+        --     bg = prompt,
+        --   }
+        --   hl.TelescopePromptBorder = {
+        --     bg = prompt,
+        --     fg = prompt,
+        --   }
+        --   hl.TelescopePromptTitle = {
+        --     bg = prompt,
+        --     fg = prompt,
+        --   }
+        --   hl.TelescopePreviewTitle = {
+        --     bg = c.bg_dark,
+        --     fg = c.bg_dark,
+        --   }
+        --   hl.TelescopeResultsTitle = {
+        --     bg = c.bg_dark,
+        --     fg = c.bg_dark,
+        --   }
+        -- end,
       }
-    end,
-    event = 'VeryLazy'
+
+      vim.cmd.colorscheme 'tokyonight'
+    end
   },
 
   {
     "sainnhe/sonokai",
     config = function()
       vim.g['sonokai_transparent_background'] = 1
-
-      -- vim.cmd.colorscheme 'sonokai'
     end,
     event = 'VeryLazy'
-  },
-
-  {
-    'ribru17/bamboo.nvim',
-    config = function()
-      require 'bamboo'.setup {
-        transparent = true
-      }
-
-      vim.cmd.colorscheme 'bamboo'
-    end,
-    -- event = 'VeryLazy'
-  },
+  }
 }
