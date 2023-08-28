1e2d565dc57b36f3a714425873e54f7fab7008e8
Jonathan Raines <jonathan.raines@apptegy.com>
2023-08-27 19:38:19 -0500

    Remove catppuccin

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,51 +3,11 @@ return {
     'folke/tokyonight.nvim',
     config = function()
       require 'tokyonight'.setup {
-        transparent = true,
-        dim_inactive = true,
+        transparent = true
       }
 
       vim.cmd.colorscheme 'tokyonight'
     end
-  },
-
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function()
-      require 'catppuccin'.setup {
-        transparent_background = true,
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          mini = true,
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-              errors = { "italic" },
-              hints = { "italic" },
-              warnings = { "italic" },
-              information = { "italic" },
-            },
-            underlines = {
-              errors = { "underline" },
-              hints = { "underline" },
-              warnings = { "underline" },
-              information = { "underline" },
-            },
-            inlay_hints = {
-              background = true,
-            },
-          },
-          treesitter = {
-            enabled = true
-          },
-          telescope = true,
-          neogit = true
-        }
-      }
-    end,
-    event = 'VeryLazy'
   },
 
   {
@@ -57,4 +17,17 @@ return {
     end,
     event = 'VeryLazy'
   },
+
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      require 'onedarkpro'.setup {
+        options = {
+          transparency = true
+        }
+      }
+    end,
+    event = 'VeryLazy'
+  }
 }
--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -6,11 +6,10 @@ return {
   keys = {
     { '<leader>gg', '<cmd>GrapplePopup tags<CR>',   desc = 'Grapple Menu' },
     { '<leader>ga', vim.cmd.GrappleTag,             desc = 'Grapple Tag' },
-    { '<leader>gh', '<cmd>GrappleSelect key=1<CR>', desc = 'Select Tag 1' },
-    { '<leader>gj', '<cmd>GrappleSelect key=2<CR>', desc = 'Select Tag 2' },
-    { '<leader>gk', '<cmd>GrappleSelect key=3<CR>', desc = 'Select Tag 3' },
-    { '<leader>gl', '<cmd>GrappleSelect key=4<CR>', desc = 'Select Tag 4' },
-    { '<leader>g;', '<cmd>GrappleSelect key=5<CR>', desc = 'Select Tag 5' },
+    { '<leader>gj', '<cmd>GrappleSelect key=1<CR>', desc = 'Select Tag 1' },
+    { '<leader>gk', '<cmd>GrappleSelect key=2<CR>', desc = 'Select Tag 2' },
+    { '<leader>gl', '<cmd>GrappleSelect key=3<CR>', desc = 'Select Tag 3' },
+    { '<leader>g;', '<cmd>GrappleSelect key=4<CR>', desc = 'Select Tag 4' },
   },
   event = 'VeryLazy'
 }
