c215a8fd302ee00e697fbe7b70a199fe2e3d39d0
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-10-23 16:30:08 -0500

    Add quickfix toggle

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -7,10 +7,11 @@ local keymap = vim.keymap.set
 local default_opts = { noremap = true, silent = true }
 local keymaps = {
   { 'n', 'J',          'mzJ`z',                                                   { desc = 'Join on same line' } },
-  -- { "n", "<leader>bd", "<cmd>bd<cr>",                                             { desc = 'Close Buffer' } },
+  -- { "n", "<leader>bd", "<cmd>bd<cr>",                                          { desc = 'Close Buffer' } },
   { "n", "<leader>L",  "<cmd>Lazy<cr>",                                           { desc = 'Lazy' } },
   { "n", "<leader>hl", "<cmd>terminal lazygit<cr>",                               { desc = 'Lazygit' } },
   { "n", "<leader>hL", "<cmd>lua require('lazy.util').float_term('lazygit')<CR>", { desc = 'Lazygit' } },
+  { 'n', '<C-q>',      require 'configs.utils'.quickfix_toggle,                   { desc = 'Move line up' } },
   { 'v', '<C-j>',      ":m '>+1<CR>gv=gv",                                        { desc = 'Move line up' } },
   { 'v', '<C-k>',      ":m '<-2<CR>gv=gv",                                        { desc = 'Move line down' } },
   { 'v', '<',          '<gv',                                                     { desc = 'Keep visual sel indent dec' } },
