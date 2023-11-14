a835501ba4320b8370ffa1e8619c49bf22433339
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-11-14 09:08:17 -0600

    Add symbol-usage

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -45,4 +45,14 @@ return {
     event = 'VeryLazy'
   },
 
+  {
+    'Wansmer/symbol-usage.nvim',
+    event = 'LspAttach',
+    config = function()
+      require('symbol-usage').setup {
+        vt_position = 'end_of_line'
+      }
+    end
+  },
+
 }
