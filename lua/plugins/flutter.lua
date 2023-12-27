807d320b80af5593f9afa70a57b613f0dbe3e99a
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-12-27 11:53:10 -0600

    Remove plugins

--- lua/plugins/flutter.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'akinsho/flutter-tools.nvim',
-  dependencies = 'nvim-lua/plenary.nvim',
-  config = function()
-    require 'flutter-tools'.setup {}
-  end,
-  ft = { 'dart' }
-}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,50 +2,6 @@ return {
   { 'nvim-tree/nvim-web-devicons' },
 
   {
-    'ribru17/bamboo.nvim',
-    config = function()
-      require('bamboo').setup { transparent = true }
-
-      -- require('bamboo').load()
-    end,
-    event = 'VeryLazy'
-  },
-
-  {
-    'dracula/vim',
-    name = 'dracula',
-    config = function()
-      vim.g['dracula_colorterm'] = 0
-
-      vim.cmd.colorscheme 'dracula'
-    end,
-    -- event = 'VeryLazy'
-  },
-
-  {
-    "sainnhe/sonokai",
-    config = function()
-      vim.g['sonokai_transparent_background'] = 1
-    end,
-    event = 'VeryLazy'
-  },
-
-  {
-    'echasnovski/mini.hipatterns',
-    version = false,
-    config = function()
-      local hipatterns = require('mini.hipatterns')
-      hipatterns.setup({
-        highlighters = {
-          -- Highlight hex color strings (`#rrggbb`) using that color
-          hex_color = hipatterns.gen_highlighter.hex_color(),
-        },
-      })
-    end,
-    event = 'VeryLazy'
-  },
-
-  {
     'Wansmer/symbol-usage.nvim',
     event = 'LspAttach',
     config = function()
