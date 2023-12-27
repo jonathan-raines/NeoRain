c4b9892fff5393a491f1e4b96fd2a4ff3b35d17b
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-12-27 12:17:18 -0600

    Fix grappler references and harpoon keymap descriptions

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,25 +1,24 @@
 return {
-    "ThePrimeagen/harpoon",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    branch = "harpoon2",
-    config = function()
-      local harpoon = require("harpoon")
+  "ThePrimeagen/harpoon",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  branch = "harpoon2",
+  config = function()
+    local harpoon = require("harpoon")
 
-      harpoon:setup()
+    harpoon:setup()
 
-      vim.keymap.set("n", "<leader>gm", function() harpoon:list():append() end)
-      vim.keymap.set("n", "<leader>gg", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<leader>gm", function() harpoon:list():append() end, { desc = 'Harpoon Mark' })
+    vim.keymap.set("n", "<leader>gg", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+      { desc = 'Harpoon Menu' })
 
-      vim.keymap.set("n", "<leader>gj", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<leader>gk", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<leader>gl", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "<leader>g;", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<leader>gj", function() harpoon:list():select(1) end, { desc = 'Harpoon Select 1' })
+    vim.keymap.set("n", "<leader>gk", function() harpoon:list():select(2) end, { desc = 'Harpoon Select 2' })
+    vim.keymap.set("n", "<leader>gl", function() harpoon:list():select(3) end, { desc = 'Harpoon Select 3' })
+    vim.keymap.set("n", "<leader>g;", function() harpoon:list():select(4) end, { desc = 'Harpoon Select 4' })
 
-      -- Toggle previous & next buffers stored within Harpoon list
-      vim.keymap.set("n", "<leader>gp", function() harpoon:list():prev() end)
-      vim.keymap.set("n", "<leader>gn", function() harpoon:list():next() end)
-    end,
-    keys = {
-      '<leader>g'
-    }
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<leader>gp", function() harpoon:list():prev() end, { desc = 'Harpoon Prev Buf' })
+    vim.keymap.set("n", "<leader>gn", function() harpoon:list():next() end, { desc = 'Harpoon Next Buf' })
+  end,
+  event = 'VeryLazy'
 }
--- lua/plugins/mini-clue.lua
+++ lua/plugins/mini-clue.lua
@@ -57,7 +57,7 @@ return {
         miniclue.gen_clues.z(),
         { mode = 'n', keys = '<Leader>b',  desc = '+Buffers' },
         { mode = 'n', keys = '<Leader>f',  desc = '+Find' },
-        { mode = 'n', keys = '<Leader>g',  desc = '+Grapple' },
+        { mode = 'n', keys = '<Leader>g',  desc = '+Harpoon' },
         { mode = 'n', keys = '<Leader>h',  desc = '+Git' },
         { mode = 'n', keys = '<Leader>ht', desc = '+Toggle' },
         { mode = 'n', keys = '<Leader>l',  desc = '+Lsp' },
