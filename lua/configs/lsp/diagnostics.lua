4e09e6c8c8dea876b8533ea8dd9a566234a2584a
Jonathan Raines <jonathan.s.raines@gmail.com>
2023-12-31 17:25:55 -0600

    Update diagnostic config

--- lua/configs/lsp/diagnostics.lua
+++ lua/configs/lsp/diagnostics.lua
@@ -1,6 +1,13 @@
--- Diagnostic Signs
-local signs = { Error = '', Warn = '', Hint = '', Info = '' }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+  float = {
+    border = 'rounded',
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.HINT] = '',
+      [vim.diagnostic.severity.INFO] = '',
+    },
+  }
+})
