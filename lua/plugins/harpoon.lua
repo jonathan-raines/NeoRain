f243323096388be06df42f1a31f0537f20412d88
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-01-03 21:08:48 -0600

    Update harpoon select keymaps

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,13 +13,17 @@ return {
     }
 
     vim.keymap.set("n", "<leader>gm", function() harpoon:list():append() end, { desc = 'Harpoon Mark' })
-    vim.keymap.set("n", "<leader>gg", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
-      { desc = 'Harpoon Menu' })
+    vim.keymap.set("n", "<leader>gg",
+      function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end,
+      { desc = 'Harpoon Menu' }
+    )
 
-    vim.keymap.set("n", "<leader>gj", function() harpoon:list():select(1) end, { desc = 'Harpoon Select 1' })
-    vim.keymap.set("n", "<leader>gk", function() harpoon:list():select(2) end, { desc = 'Harpoon Select 2' })
-    vim.keymap.set("n", "<leader>gl", function() harpoon:list():select(3) end, { desc = 'Harpoon Select 3' })
-    vim.keymap.set("n", "<leader>g;", function() harpoon:list():select(4) end, { desc = 'Harpoon Select 4' })
+    for i = 1, 9, 1 do
+      print(i)
+      vim.keymap.set("n", "<leader>g" .. i, function() harpoon:list():select(i) end, { desc = 'Harpoon Select ' .. i })
+    end
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set("n", "<leader>gp", function() harpoon:list():prev() end, { desc = 'Harpoon Prev Buf' })
