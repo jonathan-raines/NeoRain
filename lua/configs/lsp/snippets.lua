33e509c7186924019660ca6e2678b579c7ad3316
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-05-02 08:50:43 -0500

    Fix native snippets jumping

--- lua/configs/lsp/snippets.lua
+++ lua/configs/lsp/snippets.lua
@@ -1,13 +1,17 @@
 vim.o.completeopt = 'menu,menuone,noselect'
 
-vim.keymap.set({ 'i', 's' }, '<C-l>', function()
-  if vim.snippet.jumpable(1) then
+vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+  if vim.snippet.active({ direction = 1 }) then
     return '<cmd>lua vim.snippet.jump(1)<cr>'
+  else
+    return '<Tab>'
   end
 end, { expr = true })
 
-vim.keymap.set({ 'i', 's' }, '<C-h>', function()
-  if vim.snippet.jumpable(-1) then
+vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+  if vim.snippet.active({ direction = -1 }) then
     return '<cmd>lua vim.snippet.jump(-1)<cr>'
+  else
+    return '<Tab>'
   end
 end, { expr = true })
