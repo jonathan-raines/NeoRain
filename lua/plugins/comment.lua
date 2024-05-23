bcecc6697e995b345d360d248fdcd50553cba93a
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-05-23 12:19:36 -0500

    Replace mini.comment with ts-comments

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,16 +1,6 @@
 return {
-  'echasnovski/mini.comment',
-  dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
-  config = function()
-    require 'mini.comment'.setup {
-      options = {
-        custom_commentstring = function()
-          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-        end
-      }
-    }
-  end,
-  keys = {
-    { 'gc', mode = { 'n', 'x' } },
-  }
+  "folke/ts-comments.nvim",
+  opts = {},
+  event = "VeryLazy",
+  enabled = vim.fn.has("nvim-0.10.0") == 1,
 }
