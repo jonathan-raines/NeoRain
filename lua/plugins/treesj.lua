c22b2f49a6f4496da298ece5fcb294a2a581a911
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-05-30 12:08:42 -0500

    Update max join lines

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,13 +1,11 @@
 return {
-  {
-    'Wansmer/treesj',
-    dependencies = 'nvim-treesitter/nvim-treesitter',
-    opts = {
-      max_join_length = 999,
-      use_default_keymaps = false,
-    },
-    keys = {
-      { 'gs', vim.cmd.TSJToggle, desc = 'Toggle Split' },
-    },
+  'Wansmer/treesj',
+  dependencies = 'nvim-treesitter/nvim-treesitter',
+  opts = {
+    max_join_length = 999999,
+    use_default_keymaps = false,
   },
+  keys = {
+    { 'gs', vim.cmd.TSJToggle, desc = 'Toggle Split' },
+  }
 }
