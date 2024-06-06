4474ff8ebbc1ee6fb70faa73c16fbd4786707eff
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-06-06 16:47:37 -0500

    Remove markdown plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,4 @@
 return {
-
-  {
-    'MeanderingProgrammer/markdown.nvim',
-    name = 'render-markdown',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    opts = {},
-    ft = { 'markdown' }
-  },
-
   {
     'brenoprata10/nvim-highlight-colors',
     opts = { render = 'virtual' },
