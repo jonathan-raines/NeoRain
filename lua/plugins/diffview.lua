77d7da2e8b695d8fdcf68820fd319de2d5186666
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-06-12 08:38:57 -0500

    Add extra diffview keybinds

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,8 +1,10 @@
 return {
   'sindrets/diffview.nvim',
-  cmd = 'DiffviewOpen',
+  cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
   keys = {
-    { '<leader>do', vim.cmd.DiffviewOpen,  desc = '[Diffview] Open' },
-    { '<leader>dc', vim.cmd.DiffviewClose, desc = '[Diffview] Close' }
+    { '<leader>do', vim.cmd.DiffviewOpen,                           desc = '[Diffview] Open' },
+    { '<leader>dc', vim.cmd.DiffviewClose,                          desc = '[Diffview] Close' },
+    { '<leader>df', function() vim.cmd.DiffviewFileHistory '%' end, desc = '[Diffview] File History' },
+    { '<leader>dh', vim.cmd.DiffviewFileHistory,                    desc = '[Diffview] History' }
   }
 }
