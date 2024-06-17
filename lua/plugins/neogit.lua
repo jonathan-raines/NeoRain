540ee364c6357c33c01067e5914938bafd751b5a
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-06-16 22:09:27 -0500

    Trying out neogit and vim-fugitive

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,11 +1,23 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",  -- required
-    "ibhagwan/fzf-lua",       -- optional
-    "sindrets/diffview.nvim", -- optional - Diff integration
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "ibhagwan/fzf-lua",     -- optional
+      "sindrets/diffview.nvim", -- optional - Diff integration
+    },
+    config = true,
+    cmd = "Neogit",
+    keys = { { '<leader>gg', vim.cmd.Neogit, desc = 'Open Neogit' } }
   },
-  config = true,
-  cmd = "Neogit",
-  keys = { { '<leader>gg', vim.cmd.Neogit, desc = 'Open Neogit' } }
+
+  {
+    'tpope/vim-fugitive',
+    cmd = { 'Git', 'G' },
+    keys = {
+      { '<leader>gs', [[:tab G<CR>]],   desc = '[Fugitive] Status' },
+      { '<leader>gb', [[:G blame<CR>]], desc = '[Fugitive] Blame' },
+    },
+  },
+
 }
