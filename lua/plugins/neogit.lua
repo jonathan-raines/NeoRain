c8d3e0df93659b583879b8bbc8b1ed1ef2d70008
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-07-29 15:52:03 -0500

    Remove neogen and fugitive

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,23 +1,11 @@
 return {
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",  -- required
-      "ibhagwan/fzf-lua",       -- optional
-      "sindrets/diffview.nvim", -- optional - Diff integration
-    },
-    config = true,
-    cmd = "Neogit",
-    keys = { { '<leader>gg', vim.cmd.Neogit, desc = 'Open Neogit' } }
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",    -- required
+    "ibhagwan/fzf-lua",         -- optional
+    "sindrets/diffview.nvim",   -- optional - Diff integration
   },
-
-  {
-    'tpope/vim-fugitive',
-    cmd = { 'Git', 'G' },
-    keys = {
-      { '<leader>gs', [[:tab G<CR>]], desc = '[Fugitive] Status' },
-      -- { '<leader>gb', [[:G blame<CR>]], desc = '[Fugitive] Blame' },
-    },
-  },
-
+  config = true,
+  cmd = "Neogit",
+  keys = { { '<leader>gg', vim.cmd.Neogit, desc = 'Open Neogit' } }
 }
