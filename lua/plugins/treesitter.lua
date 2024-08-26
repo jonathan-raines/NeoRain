23d066f0d8c5b2450f5ac03b91c81f7326e2ef49
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-08-26 16:06:30 -0500

    Update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,6 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
-      version = "*", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "ruby" },
-      sync_install = false,
       auto_install = true,
       highlight = {
         enable = true,
