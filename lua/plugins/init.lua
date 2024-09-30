6f1d797bf1ba80cadf4df69ac08a682a3c617d5a
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-09-30 17:04:09 -0500

    Testing oil and mini.files

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,29 @@
 return {}
+return {
+  {
+    'stevearc/oil.nvim',
+    dependencies = "echasnovski/mini.icons",
+    init = function()
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end,
+    config = function()
+      require 'oil'.setup {}
+    end
+  },
+
+  {
+    'echasnovski/mini.files',
+    config = function()
+      require 'mini.files'.setup {}
+    end,
+    keys = {
+      { '<leader>e', '<cmd>lua MiniFiles.open()<CR>', desc = 'File Explorer' },
+      -- {
+      --   '-',
+      --   '<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0), true)<CR>',
+      --   desc = 'Mini Files Current Directory'
+      -- }
+    }
+  },
+
+}
