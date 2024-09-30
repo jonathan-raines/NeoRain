d7cf31aec53f2256318e7dd1983f216b2c11713c
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-09-30 17:08:15 -0500

    Disable netrw plugin

--- init.lua
+++ init.lua
@@ -38,7 +38,7 @@ require("lazy").setup('plugins', {
     rtp = {
       disabled_plugins = {
         'gzip',
-        -- 'netrwPlugin',
+        'netrwPlugin',
         'rplugin',
         'tarPlugin',
         'tohtml',
