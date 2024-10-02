dfc759658dd0e84b09b87ca0f24fb0f8db500da5
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-10-01 20:30:37 -0500

    Fix duplicate key in table

--- lua/lsp/server_settings.lua
+++ lua/lsp/server_settings.lua
@@ -25,19 +25,17 @@ local servers = {
         diagnostics = {
           enable = true,
           globals = { 'vim', 'use' },
+          disable = { 'missing-fields' }
         },
         hint = { enable = true },
         runtime = { version = 'LuaJIT' },
         workspace = {
           checkThirdParty = false,
-          -- Tells lua_ls where to find all the Lua files that you have loaded
           library = { vim.env.VIMRUNTIME },
         },
         completion = {
           callSnippet = 'Replace',
         },
-        -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-        diagnostics = { disable = { 'missing-fields' } },
         telemetry = { enable = false },
       },
     },
