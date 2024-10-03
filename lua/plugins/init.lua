499266891370c1c62824c4324459a0b27980a41e
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-10-03 08:52:13 -0500

    Revert some previous changes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,1 @@
-return {
-  {
-    'stevearc/oil.nvim',
-    dependencies = "echasnovski/mini.icons",
-    config = function()
-      require 'oil'.setup {}
-    end,
-    keys = {
-      { "-", "<CMD>Oil<CR>", desc = "Open parent directory" }
-    }
-  }
-
-}
+return {}
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,29 +1,13 @@
 return {
   {
-    'echasnovski/mini.files',
-    config = function()
-      require 'mini.files'.setup {}
-    end,
-    keys = {
-      { '<leader>e', '<cmd>lua MiniFiles.open()<CR>', desc = 'File Explorer' },
-      -- {
-      --   '-',
-      --   '<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0), true)<CR>',
-      --   desc = 'Mini Files Current Directory'
-      -- }
-    }
-  },
-
-  {
     "echasnovski/mini.icons",
-    opts = {},
-    lazy = true,
     init = function()
       package.preload["nvim-web-devicons"] = function()
         require("mini.icons").mock_nvim_web_devicons()
         return package.loaded["nvim-web-devicons"]
       end
     end,
-  },
+    opts = {}
+  }
 
 }
