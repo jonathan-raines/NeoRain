db72b10be0c488f65cb805b6d2ad1d2495388adc
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-11-11 15:04:37 -0600

    Update netrw settings

--- lua/options.lua
+++ lua/options.lua
@@ -78,8 +78,7 @@ opt.formatoptions:remove "o"
 opt.iskeyword:append("-")
 
 -- NETRW --
-vim.g.netrw_altfile   = 1
-vim.g.netrw_banner    = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_preview   = 1
-vim.g.netrw_winsize   = 15
+vim.g.netrw_altfile = 1
+vim.g.netrw_banner  = 0
+vim.g.netrw_preview = 1
+vim.g.netrw_winsize = 15
