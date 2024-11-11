3f61c8cb8bd12333928aacc1bc1f361b28abc6a3
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-11-11 15:04:08 -0600

    Update colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -8,6 +8,25 @@ return {
         }
       }
     end
+  },
+
+  {
+    'sainnhe/sonokai',
+    config = function()
+      vim.g.sonokai_transparent_background = true
+    end,
+    lazy = true
+  },
+
+  {
+    "wtfox/jellybeans.nvim",
+    priority = 1000,
+    config = function()
+      require("jellybeans").setup {
+        transparent = true
+      }
+    end,
+    lazy = true
   }
 
 }
