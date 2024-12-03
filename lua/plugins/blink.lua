f7f0ff4a9a23d56cb468907e74250cf3edf0b9a0
Jonathan Raines <jonathan.s.raines@gmail.com>
2024-12-03 15:18:02 -0600

    Update blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -12,28 +12,42 @@ return {
     -- If you use nix, you can build from source using latest nightly rust with:
     -- build = 'nix run .#build-plugin',
 
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
       -- 'default' for mappings similar to built-in completion
       -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- see the "default configuration" section below for full documentation on how to define
-      -- your own keymap. when defining your own, no keybinds will be assigned automatically.
-      keymap = 'default',
+      -- your own keymap.
+      keymap = { preset = 'default' },
 
-      highlight = {
-        -- sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- useful for when your theme doesn't support blink.cmp
-        -- will be removed in a future release, assuming themes add support
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release
         use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
       },
-      -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'normal',
+
+      -- default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, via `opts_extend`
+      sources = {
+        completion = {
+          enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
+        },
+      },
 
       -- experimental auto-brackets support
-      -- accept = { auto_brackets = { enabled = true } }
+      -- completion = { accept = { auto_brackets = { enabled = true } } }
 
       -- experimental signature help support
-      -- trigger = { signature_help = { enabled = true } }
-    }
-  }
+      -- signature = { enabled = true }
+    },
+    -- allows extending the enabled_providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.completion.enabled_providers" }
+  },
 }
